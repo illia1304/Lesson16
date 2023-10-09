cba3f6dbfe41ba0d74245807c7668005e3b8c6e9
Illia <1304illia@gmail.com>
2023-10-10 01:30:21 +0300

    ElectionsManegment, Vote

--- Lesson16/Program.cs
+++ Lesson16/Program.cs
@@ -1,9 +1,43 @@
 ﻿namespace Lesson16
 {
-    public class Vote
+    public class ElectionsManegment
+    {
+        public Dictionary<string, int> Votes = new();
+        public ElectionsManegment()
+        {
+        }
+
+        public void CreateTopic(string name)
+        {
+            Console.WriteLine("Enter name of the topic:");
+            name = Console.ReadLine();
+        }
+
+        public void CreateOptions(string name)
+        {
+            name = Console.ReadLine();
+            Votes.Add(name, 0);
+        }
+
+    }
+
+    public class Vote : ElectionsManegment
     {
         public Vote()
         {
+
+        }
+      
+        public void VoteFor(string optionName)
+        {
+            if(Votes.ContainsKey(optionName))
+            {
+                Votes[optionName] += 1;
+            }
+            else
+            {
+                Console.WriteLine("You are invalid)))");
+            }
         }
     }
 
