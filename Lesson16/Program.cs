4457c6b1567f7c88560ad6cf3744131c66e801c2
Illia <1304illia@gmail.com>
2023-10-10 01:18:56 +0300

    Add project files.

---
+++ Lesson16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson16
+{
+    public class Vote
+    {
+        public Vote()
+        {
+        }
+    }
+
+    class Programe
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
